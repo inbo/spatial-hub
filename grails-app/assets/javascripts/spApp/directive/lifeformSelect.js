f360f151e4b6340352072ed69431f9bcab7e2bdd
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-05-08 13:38:32 +0200

    inbo/vlaams-biodiversiteitsportaal#901: Try translating species groups

--- grails-app/assets/javascripts/spApp/directive/lifeformSelect.js
+++ grails-app/assets/javascripts/spApp/directive/lifeformSelect.js
@@ -1,32 +1,38 @@
 (function (angular) {
-    'use strict';
-    /**
-     * @memberof spApp
-     * @ngdoc directive
-     * @name lifeformSelect
-     * @description
-     *   Select dropdown of biocache lifeforms
-     */
-    angular.module('lifeform-select-directive', []).directive('lifeformSelect',
-        ['BiocacheService', function (biocacheService) {
-            return {
-                scope: {
-                    _custom: '&onCustom'
-                },
-                template: "<select ng-model='selectedItem' ng-change='selectItem(selectedItem)'><option ng-repeat='x in list' value='{{x.query}}'>{{x.label}}</option></select>",
-                link: function (scope, element, attrs) {
-                    scope.list = [];
-                    scope.selectItem = function (selectedItem) {
-                        scope._custom()(JSON.parse(selectedItem))
-                    };
+  "use strict";
+  /**
+   * @memberof spApp
+   * @ngdoc directive
+   * @name lifeformSelect
+   * @description
+   *   Select dropdown of biocache lifeforms
+   */
+  angular.module("lifeform-select-directive", []).directive("lifeformSelect", [
+    "BiocacheService",
+    function (biocacheService) {
+      return {
+        scope: {
+          _custom: "&onCustom",
+        },
+        template:
+          "<select ng-model='selectedItem' ng-change='selectItem(selectedItem)'><option ng-repeat='x in list' value='{{x.query}}' i18n='{{x.label}}'>{{x.label}}</option></select>",
+        link: function (scope, element, attrs) {
+          scope.list = [];
+          scope.selectItem = function (selectedItem) {
+            scope._custom()(JSON.parse(selectedItem));
+          };
 
-                    biocacheService.facetGeneral('species_group', biocacheService.newQuery(), -1, 0).then(function (data) {
-                        $.each(data[0].fieldResult, function (idx, item) {
-                            item.query = biocacheService.newQuery([item.fq], item.label);
-                            scope.list.push(item)
-                        });
-                    })
-                }
-            };
-        }])
-}(angular));
+          biocacheService
+            .facetGeneral("species_group", biocacheService.newQuery(), -1, 0)
+            .then(function (data) {
+              $.each(data[0].fieldResult, function (idx, item) {
+                item.query = biocacheService.newQuery([item.fq], item.label);
+                scope.list.push(item);
+              });
+            });
+        },
+      };
+    },
+  ]);
+})(angular);
+
